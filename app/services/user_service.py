97b6928fe41e916ce3d5bbcd4290c8bd366fad65
xdetre <enderctoun@gmail.com>
2026-03-30 00:12:21 +0300

    added method for creating googl users

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -18,3 +18,10 @@ async def get_user_by_email(db: AsyncSession, email: str):
 
     result = await db.execute(select(User).where(User.email == email))
     return result.scalar_one_or_none()
+
+async def create_google_user(db: AsyncSession, email: str):
+    user = User(email=email, password_hash=None)
+    db.add(user)
+    await db.commit()
+    await db.refresh(user)
+    return user
