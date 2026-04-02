4c4ce68dc6d1c803f2d48241a9ad3c48c35badb9
xdetre <enderctoun@gmail.com>
2026-04-02 22:49:58 +0300

    added resend api key

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,8 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
     DOMAIN: str
+    RESEND_API_KEY: str
+
 
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
