8d568f9c31f1a8d4ddba46ed7cca346a2280c963
xdetre <enderctoun@gmail.com>
2026-04-02 22:58:11 +0300

    new data for the table

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, DateTime
+from sqlalchemy import String, DateTime, Boolean
 from app.db.session import Base
 from datetime import datetime
 from typing import Optional
@@ -10,6 +10,9 @@ class User(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     email: Mapped[str] = mapped_column(String, unique=True, index=True)
     password_hash: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    is_verified: Mapped[bool] = mapped_column(Boolean, default=False)
+    verification_code: Mapped[Optional[str]] = mapped_column(String, nullable=True)
+    code_sent_at: Mapped[Optional[datetime]] = mapped_column(DateTime, nullable=True)
     created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.utcnow)
 
     aliases = relationship("Alias", back_populates="user")
